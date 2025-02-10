e744a7725ed51cb04710c5f1fd7d8adee8995c70
Manmoksh <mamoksh04@gmail.com>
2025-02-10 17:23:51 +0530

    custom header

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,21 +6,16 @@ import React from "react";
 const Header = ({ children, className }: HeaderProps) => {
   return (
     <header className={cn("header", className)}>
-      <Link href="/" className="md:flex-1">
-        <Image
-          src="/assets/icons/logo.svg"
-          alt="Logo with name"
-          width={120}
-          height={32}
-          className="hidden md:block w-auto h-auto"
-        />
+      <Link href="/" className="md:flex-1 flex items-center">
         <Image
           src="/assets/icons/logo-icon.svg"
           alt="Logo"
           width={32}
           height={32}
-          className="mr-2 md:hidden"
         />
+        <h1 className="text-white text-xl font-medium hidden md:block">
+          DocCollabs
+        </h1>
       </Link>
       {children}
     </header>
